70f04612c1a46e7e442f2407d3b87629854817c8
Loan LEPINEY <llepiney@e2r11p17.clusters.42paris.fr>
2022-04-28 19:00:03 +0200

    first push

---
+++ wall_check.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   walls.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llepiney <llepiney@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/28 18:07:09 by llepiney          #+#    #+#             */
+/*   Updated: 2022/04/28 18:32:32 by llepiney         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+
+int	line_numb(char **map)
+{
+	int	size;
+
+	size = 0;
+	while (map[size])
+		size++;
+	return (size);
+}
+
+int	col_numb(char **map)
+{
+	int	size;
+
+	size = 0;
+	while (map[0][size])
+		size++;
+	return (size);
+}
+
+int	wall_valid(char **map)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if ((i == 0 || i == (line_numb(map) - 1)) && map[i][j] != 'w')
+				return (0);
+			if ((j == 0 || j == (col_num(map) - 1)) && map[i][j] != 'w')
+				return (0);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	is_rect(char **map)
+{
+	int	i;
+	int	j;
+	int	lines;
+	int	cols;
+
+	i = 0;
+	lines = line_numb(map);
+	cols = col_numb(map);
+	while (map(i))
+	{
+		j = 0;
+		while (map[i][j])
+			j++;
+		if (j != cols)
+			return (0);
+		i++;
+	}
+	if (i != lines)
+		return (0);
+	return (1);
+}
